dfc05edd023d11cdf6665bc8cfaddbecad26f15c
Waithira <50845913+Doroooh@users.noreply.github.com>
2024-11-08 00:06:44 +0300

    4-app.py

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,37 +1,35 @@
 #!/usr/bin/env python3
-""" a basic flask app"""
+"""Simple flask app setup"""
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
-
+from flask_babel import Babel
 app = Flask(__name__)
-
-
-class Config(object):
-    """ Config class for Babel object """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
-
-app.config.from_object(Config)
 babel = Babel(app)
 
 
-@app.route('/')
-def hello():
-    """ render a basic html file """
-    return render_template('4-index.html')
+class Config(object):
+    """Class to set defualts for babel"""
+    LANGUAGES = ["en", "fr"]
+    Babel.default_locale = "en"
+    Babel.default_timezone = "UTC"
+
+
+app.config.from_object(Config)
 
 
 @babel.localeselector
 def get_locale():
-    """ a function to determine the best match with the supported languages """
-    if request.full_path.split('/')[1][:8] == "?locale=":
-        lg = request.full_path.split('/')[1][8:]
-        if lg in app.config['LANGUAGES']:
-            return lg
+    """Get best langauge for user"""
+    locale = request.args.get('locale')
+    if locale and locale in Config.LANGUAGES:
+        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
+@app.route('/')
+def index():
+    """Return simple homepage"""
+    return render_template('4-index.html')
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host='0.0.0.0', port=8000)
