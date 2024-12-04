67f50d87ee6b76ebbec7549bc44a7b11049ae5ed
Waithira <50845913+Doroooh@users.noreply.github.com>
2024-12-04 23:43:23 +0300

    Queuing system

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,10 +1,15 @@
+// Import the Redis client library
 import redis from 'redis';
+
+// Create a new Redis client instance
 const client = redis.createClient();
 
+// Event listener for successful connection to the Redis server
 client.on('connect', function() {
-    console.log('Redis client connected to the server');
+    console.log('Redis client connected to the server'); // Log a message when the connection is successful
 });
 
+// Event listener for errors during the connection or operation with the Redis server
 client.on('error', function(error) {
-  console.error(`Redis client not connected to the server: ${error.message}`);
+    console.error(`Redis client not connected to the server: ${error.message}`); // Log the error message if an issue occurs
 });
